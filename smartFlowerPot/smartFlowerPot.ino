d4cb268c432a49f71dae5b2d4857cb30e4930366
Ian Valdovinos <ian.valdovinos752001@gmail.com>
2020-10-06 20:35:04 -0400

    Refactor to one function call to check the sensor status

--- smartFlowerPot/smartFlowerPot.ino
+++ smartFlowerPot/smartFlowerPot.ino
@@ -1,27 +1,60 @@
 const int sensor1 = 2;
 const int sensor2 = 3;
 const int sensor3 = 4;
+int sensor1State = 0;         
+int sensor2State = 0;
+int sensor3State = 0;
 
-const int LED1 = 14;
-const int LED2 = 16;
-const int LED3 = 10;
+const int LEDRed = 14;
+const int LEDGreen = 16;
+const int LEDBlue = 10;
 
 const int waterPump = 8; 
+
+int lastColorHit = 0;
 
 void setup() {
   pinMode(sensor1, INPUT_PULLUP);
   pinMode(sensor2, INPUT_PULLUP);
   pinMode(sensor3, INPUT_PULLUP);
 
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
+  pinMode(LEDRed, OUTPUT);
+  pinMode(LEDBlue, OUTPUT);
+  pinMode(LEDGreen, OUTPUT);
 
   pinMode(waterPump, OUTPUT);
   
 }
 
 void loop() {
-  
+   
+  checkSensorStatus();
+  delay(3000);
 
+}
+
+void checkSensorStatus() {
+  sensor1State = digitalRead(sensor1);
+  sensor2State = digitalRead(sensor2);
+  sensor3State = digitalRead(sensor3);
+
+  if (sensor1State == LOW) {
+    updateColor(lastColorHit, LEDRed);
+    lastColorHit = LEDRed;
+  } 
+
+  if (sensor2State == LOW) {
+    updateColor(lastColorHit, LEDBlue);
+    lastColorHit = LEDBlue;
+  } 
+
+  if (sensor3State == LOW) {
+    updateColor(lastColorHit, LEDGreen);
+    lastColorHit = LEDGreen;
+  }
+}
+
+void updateColor(int lastColor, int newColor){
+  digitalWrite(lastColor, LOW);
+  digitalWrite(newColor, HIGH);
 }
